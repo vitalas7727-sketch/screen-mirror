cf9c4f552fffd762d09e94802f327d7f26f1b7a4
vitalas7727-sketch <vitalas7727@gmail.com>
2026-09-16 16:12:52 +0500

    Update MainActivity.java

--- app/src/main/java/com/vitaliy/screenmirror/MainActivity.java
+++ app/src/main/java/com/vitaliy/screenmirror/MainActivity.java
@@ -80,6 +80,7 @@ public class MainActivity extends Activity {
                 && resultCode == RESULT_OK
                 && data != null) {
             statusText.setText("OK: RESULT + DATA");
+            
 
             Intent serviceIntent =
                     new Intent(
