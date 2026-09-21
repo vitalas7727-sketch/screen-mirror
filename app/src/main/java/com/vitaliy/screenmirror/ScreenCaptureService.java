669033de53e65473e9de9ff21d1092f56cbfaf8a
vitalas7727-sketch <vitalas7727@gmail.com>
2026-09-21 13:22:34 +0500

    Update ScreenCaptureService.java

--- app/src/main/java/com/vitaliy/screenmirror/ScreenCaptureService.java
+++ app/src/main/java/com/vitaliy/screenmirror/ScreenCaptureService.java
@@ -696,21 +696,10 @@ if (width > 720) {
             ".textContent='SERVER_ERROR';" +
             "});" +
             "}" +
-            function nextFrame(){
-    var screen =
-        document.getElementById('screen');
-
-    screen.onload = function(){
-        setTimeout(nextFrame, 10);
-    };
-
-    screen.src =
-        '/frame?t=' + Date.now();
-}
-
-nextFrame();
-updateStatus();
-setInterval(updateStatus,1000);
+            "updateFrame();" +
+"updateStatus();" +
+"setInterval(updateFrame,100);" +
+"setInterval(updateStatus,1000);" +
             "</script>" +
             "</body>" +
             "</html>";
