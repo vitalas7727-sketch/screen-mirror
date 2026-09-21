605c45a5dd617a2baf409f66901aafd977ea5a18
vitalas7727-sketch <vitalas7727@gmail.com>
2026-09-21 19:48:34 +0500

    Update ScreenCaptureService.java

--- app/src/main/java/com/vitaliy/screenmirror/ScreenCaptureService.java
+++ app/src/main/java/com/vitaliy/screenmirror/ScreenCaptureService.java
@@ -698,7 +698,7 @@ if (width > 720) {
             "}" +
             "updateFrame();" +
 "updateStatus();" +
-"setInterval(updateFrame,100);" +
+"setInterval(updateFrame,50);" +
 "setInterval(updateStatus,1000);" +
             "</script>" +
             "</body>" +
